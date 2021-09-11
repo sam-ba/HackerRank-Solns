6514471e2c68d7888cf72d83bbd1c602ee5cd61d
Samuel-Biokpo Asimiea <biokposamuel.asimiea@ontariotechu.net>
2021-09-10 21:53:20 -0400

    started the algorithm. issue with debugging

--- Problem Solving/plusMinus.cpp
+++ Problem Solving/plusMinus.cpp
@@ -1,9 +1,39 @@
-/* Given an array of integers, calculate the ratio of its elements that are positive, negative, and zero. 
+/* Given an array of integers entered by the user, calculate the ratio of its elements that are positive, negative, and zero. 
  * Print the value of each fraction on a new line with 6 places after the decimal
 */
 
+#include <iostream>
+#include <iomanip>
+
 using namespace std; 
 
+// Variable to hold the number of integers
+int j = 0;
+
+// Variable to hold the user's input
+int a = 0;
+
+// Variables that count the no of positive, negative, and zero integers
+float pos, neg, zer = 0;
+
 int main(){
+    cout << "How many integers are you entering?" << endl;
+    cin >> j;
     
+    cout << "Enter a positive, negative, or zero integer: " << endl;
+    cin >> a;
+
+    if(a > 0){
+        pos++;
+    }
+
+    else if(a < 0){
+        neg++;
+    }
+
+    else{
+        zer++;
+    }
+
+    cout << "The ratio of positive integers is: " << pos/j.setprecision(6) << endl;
 }
