11db525e4576ebbecb0f68bd881b0991668ffd1c
Samuel-Biokpo Asimiea <biokposamuel.asimiea@ontariotechu.net>
2021-09-10 20:48:18 -0400

    started the plusMinus challenge

---
+++ Problem Solving/plusMinus.cpp
@@ -0,0 +1,9 @@
+/* Given an array of integers, calculate the ratio of its elements that are positive, negative, and zero. 
+ * Print the value of each fraction on a new line with 6 places after the decimal
+*/
+
+using namespace std; 
+
+int main(){
+    
+}
