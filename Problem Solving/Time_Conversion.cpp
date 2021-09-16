b730280ad23cc41f6fb7f57fc5c9a40efbdded51
Samuel-Biokpo Asimiea <biokposamuel.asimiea@ontariotechu.net>
2021-09-15 20:42:47 -0400

    defined the challenge

---
+++ Problem Solving/Time_Conversion.cpp
@@ -0,0 +1,44 @@
+/*
+From: https://www.hackerrank.com/challenges/one-month-preparation-kit-time-conversion/problem?h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+
+Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock. 
+      - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+s = '12:01:00PM'
+Return '12:01:00'.
+s = '12:01:00AM'
+Return '00:01:00'.
+
+Function Description
+It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+string s: a time in 12 hour format
+
+Returns
+string: the time in 24 hour format
+
+Input Format
+A single string s that represents a time in 12-hour clock format (i.e. hh:mm:ssAM or hh:mm:ssPM).
+
+Constraints
+All input times are valid
+
+Sample Input
+07:05:45PM
+
+Sample Output
+19:05:45
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
